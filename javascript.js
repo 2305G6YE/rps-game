f3468c90fc4a0ee777c6e1fd36cdaa86b412b5fd
2305G6YE <ulmi910@gmail.com>
2025-05-12 12:11:27 +0900

    Add: 70% Pseudocode for playRound function

--- javascript.js
+++ javascript.js
@@ -22,5 +22,19 @@ function getHumanChoice() {
      //Prompt the user for a input of one of the following three: rock. paper or scissors
     const choice = prompt("Rock, Paper or Scissors?");
 }
-console.log(getHumanChoice());
-    
+
+//CREATE two variables named humanScore and computerScore
+//Initialize them to both start at 0
+let humanScore = 0;
+let computerScore = 0;
+
+//CREATE a function named playRound, defining parameters as humanChoice and computerChoice
+    //Make humanChoice case-insensitive by converting input to upper case and using that during loops
+    //IF computerChoice is paper
+        //IF humanChoice is paper, return "Draw."
+        //ELSE IF humanChoice is rock, return "You Lose."
+        //ELSE IF humanChoice is scissors, return "You Win!"
+    //IF  computerchoice is rock
+        //IF humanChoice is paper, return "You Win!"
+        //IF humanChoice is rock, return "Draw."
+        //IF humanChoice is scissors, return "You Lose."
