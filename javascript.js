cd109595129f923f916bd0b62307904fa2e00acc
2305G6YE <ulmi910@gmail.com>
2025-05-12 11:55:11 +0900

    Fix: logical operator error printing paper for both paper and scissor conditions

--- javascript.js
+++ javascript.js
@@ -1,9 +1,19 @@
-/* 
-CREATE function named getComputerChoice
-    randomly returns a number using Math.random
-    IF number is below or equal to 0.33, return "rock"
-    ELSE IF number is between 0.34 and 0.66, return "paper"
-    ELSE return "scissors"
-
-CREATE function named getHumanChoice
-    input user's choice using prompt
+ 
+//CREATE function named getComputerChoice
+function getComputerChoice() {
+    //randomly returns a number using Math.random
+    const decimalNumber = Math.random();
+    const result = decimalNumber.toFixed(5);
+    console.log(result);
+    //IF number is below or equal to 0.33, return "rock"
+    if(result <= 0.33) {
+        return "rock"; 
+        // ELSE IF number is between 0.34 and 0.66, return "paper"
+        } else if (result > 0.33 && result < 0.67 ) {
+        return "paper";
+        // ELSE return "scissors"
+        } else if (result => 0.67) {
+        return "scissors";
+        }
+}
+console.log(getComputerChoice());
