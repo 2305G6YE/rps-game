0bf352f51618ca331172bcd9214e700aa774e0ad
2305G6YE <ulmi910@gmail.com>
2025-05-12 10:30:20 +0900

    Add: pseudocode for getComputerChoice function

---
+++ javascript.js
@@ -0,0 +1,9 @@
+/* 
+CREATE function named getComputerChoice
+    randomly returns a number using Math.random
+    IF number is below or equal to 0.33, return "rock"
+    ELSE IF number is between 0.34 and 0.66, return "paper"
+    ELSE return "scissors"
+
+CREATE function named getHumanChoice
+    input user's choice using prompt
