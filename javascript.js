d20d319a3155f0f75e00acde989943ce8a805518
2305G6YE <ulmi910@gmail.com>
2025-05-13 10:30:06 +0900

    Add: Psuedocode for playRound, playGame. Update getHumanChoice function for all variations of choice

--- javascript.js
+++ javascript.js
@@ -21,6 +21,9 @@ function getComputerChoice() {
 function getHumanChoice() {
      //Prompt the user for a input of one of the following three: rock. paper or scissors
     const choice = prompt("Rock, Paper or Scissors?");
+    //Make humanChoice case-insensitive by converting input to lower case and using that during loops
+    const choice2 = choice.toLowerCase();
+    return choice2;
 }
 
 //CREATE two variables named humanScore and computerScore
@@ -28,13 +31,37 @@ function getHumanChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
+//CREATE a const humanSelection and const computerSelection
+
 //CREATE a function named playRound, defining parameters as humanChoice and computerChoice
-    //Make humanChoice case-insensitive by converting input to upper case and using that during loops
     //IF computerChoice is paper
+    if(getComputerChoice() = "paper") {
         //IF humanChoice is paper, return "Draw."
+        if(getHumanChoice() = )
         //ELSE IF humanChoice is rock, return "You Lose."
+            //increment computerScore by 1
         //ELSE IF humanChoice is scissors, return "You Win!"
+            //increment humanScore by 1
+    }
+        
     //IF  computerchoice is rock
         //IF humanChoice is paper, return "You Win!"
-        //IF humanChoice is rock, return "Draw."
-        //IF humanChoice is scissors, return "You Lose."
+            //increment humanScore by 1
+        //ELSE IF humanChoice is rock, return "Draw."
+        //ELSE IF humanChoice is scissors, return "You Lose."
+            //increment computerScore by 1
+    //IF computerChoice is scissors
+        //ELSE IF humanChoice is paper, return "You Lose."
+            //increment computerScore by 1
+        //ELSE IF humanChoice is rock, return "You Win!"
+            //increment humanScore by 1
+        //ELSE IF humanChoice is scissors, return "Draw.
+    //
+
+//CREATE a function named playGame
+    //WHILE starting at 0, until counter variable is at 5, run function playRound.
+    //CONSOLE log both computerScore and humanScore.
+    //IF computerScore > humanScore
+        //PRINT "You Lost!"
+    //ELSE IF 
+        //
